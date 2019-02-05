f36f322ab260eb654524267982734c414c09a311
Farsos Bulsara <kingsukhoi@gmail.com>
2019-02-04 20:37:57 -0700

    currently:  pulls config from json file  replaces the zone name with cf's zone id  replaces dns names with dns cf's dns ids

--- driver.py
+++ driver.py
@@ -2,48 +2,75 @@
 
 # cli option(s) [1] csv location
 import sys
-import urllib.request
-import urllib.parse
-import base64
+import requests
+import json
+
+email = ''
+api_key = ''
+ip = ''
+dns_entries = []
+zone_id = ''
 
 
-# https://stackoverflow.com/questions/13261802/converting-a-string-to-and-from-base-64/13267801
-def string_to_base64_string(s):
-    s2 = base64.b64encode(s.encode('UTF-8')).decode('UTF-8')
-    return s2
+# This script will receive the names of the entries from the file , and replace them with the Cloud Flare ID's
 
 
-# https://username:password@domains.google.com/nic/update?hostname=subdomain.yourdomain.com&myip=1.2.3.4
+# make a new cloudflare request
+def get_headers():
+    return {"X-Auth-Email": email, "X-Auth-Key": api_key}
 
-def make_request(username, password, fully_qualified_domain_name, ip):
-    url = f"https://domains.google.com/nic/update?hostname={fully_qualified_domain_name}&myip={ip}"
-    my_request = urllib.request.Request(url)
-    auth = '%s:%s' % (username, password)
-    auth_string = "Basic %s" % string_to_base64_string(auth)
-    my_request.add_header("Authorization", auth_string)
-    result = urllib.request.urlopen(my_request).read().decode('UTF-8')
-    return result
+
+# get zone id
+def get_zone():
+    url = "https://api.cloudflare.com/client/v4/zones/"
+    response = requests.get(url, headers=get_headers())
+    data = json.loads(response.text)
+    global zone_id
+    for elem in data["result"]:
+        if elem['name'] == zone_id:
+            zone_id = elem['id']
+
+
+def get_record_ids():
+    url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
+    response = requests.get(url, headers=get_headers())
+    data = json.loads(response.text)
+    data = data["result"]
+    new_entries = []
+    global dns_entries
+    for curr in data:
+        if curr['name'] in dns_entries:
+            new_entries.append(curr["id"])
+    dns_entries = new_entries
 
 
 def get_ip():
-    return urllib.request.urlopen("https://api.ipify.org").read().decode('UTF-8')
+    global ip
+    ip = requests.get("https://api.ipify.org").text
 
 
-# [0] username [1] password [2] fullyQualifiedName
-def read_and_make_requests(file, ip):
-    with open(file, 'r') as file:
-        # trash first line because that's a header
-        file.readline()
-        for line in file:
-            use_this = line.split(',')
-            print(make_request(use_this[0].strip(), use_this[1].strip(), use_this[2].strip(), ip))
+# json tutorial https://realpython.com/python-json/#python-supports-json-natively
+def read_config(file_loc):
+    with open(file_loc, 'r') as file:
+        data = json.loads(file.read())
+        global email
+        email = data["email"]
+        global api_key
+        api_key = data["api_key"]
+        global zone_id
+        zone_id = data["zone"]["name"]
+        global dns_entries
+        dns_entries = data["zone"]["A"]
 
 
 def main():
     # currently assuming there is a header and I'm ignoring it.
     if len(sys.argv) != 2:
-        sys.exit('Either to few arguments or too many arguments. I need a csv file location')
-    read_and_make_requests(sys.argv[1], get_ip())
+        sys.exit('Either to few arguments or too many arguments. I need the json config file location')
+    get_ip()
+    read_config(sys.argv[1])
+    get_zone()
+    get_record_ids()
 
 
 main()
